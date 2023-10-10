914a60d877593b6d78f5dc0c1f6754d7283c8048
Sathya-9353 <97302550+Sathya-9353@users.noreply.github.com>
2023-10-09 22:40:16 -0700

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,20 @@
+package com.edu;
+
+//import java.util.Scanner;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+//		System.out.println("Hello world");
+		
+		//scanner 
+//		Scanner sc = new Scanner(System.in);  //object
+//		//System.out.println(name);
+//		String name = sc.nextLine();
+//		System.out.println(name);
+		int a= 10;
+		int b=20;
+		System.out.println("Addition "+a+b);
+	}
+
+}
