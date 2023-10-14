5e5abb6b8a26719b2c8efc881c24c928e1885ca0
Sathya-9353 <97302550+Sathya-9353@users.noreply.github.com>
2023-10-14 00:05:39 -0700

    Add files via upload

---
+++ Examples1Do.java
@@ -0,0 +1,37 @@
+package LoopingStatements;
+
+public class Examples1Do {
+
+	public static void main(String[] args) {
+		////1
+//		do
+//			System.out.println();
+//		while(true);
+		
+		//2
+//		do
+//			int x=10;   //it wont allow declaration
+//		while(true);
+		
+		//3
+//		do {
+//			int x=10;
+//			System.out.println("Hello");
+//		}
+//		while(false);
+		
+		//4
+		do
+		while(true)
+			System.out.println("Hello");
+		while(false);
+		
+		//5
+//		do
+//			System.out.println("Hello");
+//		while(true)
+//			System.out.println("hello");
+//		while(false);
+	}
+
+}
